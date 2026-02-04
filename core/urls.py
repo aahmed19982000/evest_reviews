15a18f9cded973f2591cc456561adf778f8cef6f
ahmed <ahmev@ahmeds-MacBook-Air.local>
2026-02-04 13:38:30 +0200

    عمل sitemap and robttext

--- core/urls.py
+++ core/urls.py
@@ -2,24 +2,40 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static 
-
-# 1. استيراد أداة الـ sitemap من دجانغو
-from django.contrib.sitemaps import views as sitemap_views # استيراد العروض
+from django.http import HttpResponse
+from django.contrib.sitemaps import views as sitemap_views
 from reviews.sitemaps import ReviewSitemap, UserSitemap
 
+# 1. تعريف الخرائط
 sitemaps = {
     'review': ReviewSitemap,
     'users': UserSitemap,
 }
 
+# 2. دالة robots.txt (يفضل وضعها قبل urlpatterns)
+def robots_txt(request):
+    content = (
+        "Sitemap: https://evestreviews.pythonanywhere.com/sitemap.xml\n\n"
+        "User-agent: *\n"
+        "Allow: /\n"
+        "Disallow: /admin/\n"
+    )
+    return HttpResponse(content, content_type="text/plain")
+
+# 3. قائمة روابط واحدة لكل شيء
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('reviews.urls')),
+    path('', include('reviews.urls')), # هذا هو السطر الذي يعيد تشغيل موقعك
     
-    # 1. رابط الفهرس الرئيسي (Index)
+    # روابط الخرائط
     path('sitemap.xml', sitemap_views.index, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.index'),
+    path('sitemap-<section>.xml', sitemap_views.sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     
-    # 2. رابط الخرائط الفرعية (سيتولد تلقائياً روابط مثل sitemap-companies.xml)
-    path('sitemap-<section>.xml', sitemap_views.sitemap, {'sitemaps': sitemaps}, 
-         name='django.contrib.sitemaps.views.sitemap'),
+    # رابط robots.txt
+    path("robots.txt", robots_txt), 
 ]
+
+# إضافة روابط الملفات الثابتة
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
